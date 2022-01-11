d5ed3c624974b99e114ea76a733dd1a6ab1591aa
Thiago Rodrigues <thiagoney@outlook.com>
2022-01-11 17:41:38 -0300

    Upload file code02_01.go

---
+++ cap02/code02_01.go
@@ -0,0 +1,21 @@
+// https://www.youtube.com/c/AprendaGo/videos
+// https://youtu.be/QT7YvrEWMGE
+// https://github.com/ellenkorbes/aprendago
+
+package main
+
+import (
+	"fmt"
+)
+
+func main (){
+		x:= 10
+		y := "Olá, bom dia."
+
+		fmt.Printf("x: %v, %T\n", x, x)
+		fmt.Printf("y: %v, %T\n\n", y, y)
+
+		x, z := 20, 30
+		fmt.Printf("x: %v, %T\n", x, x)
+		fmt.Printf("z: %v, %T\n", z, z)
+}
